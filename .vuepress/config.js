c2302050032028f3863355cd2dee021f425dcda6
haitaodesign <15393243613@163.com>
2019-07-16 23:19:48 +0800

    feat: 新增配置文件

---
+++ .vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  title: '前端知识体系搭建',
+  description: 'web前端，跨终端，小程序，node，devops',
+  head: [
+    ['link', { rel: 'shortcut icon', href: '/favicon.ico'}]
+  ],
+  serviceWorker: true,
+  port: 8081,
+  base: '/learning-resource/',
+  dest: 'docs'
+}
