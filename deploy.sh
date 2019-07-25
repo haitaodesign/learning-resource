14f178ffae5ce79f8b22eb76c8853ec223760a7c
haitaodesign <15393243613@163.com>
2019-07-25 23:59:56 +0800

    ci: 新增 deploy.sh 脚本

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+yarn build
+
+git add .
+
+git commit -m 'release: github pages'
+
+git config --local user.name "haitaodesign"
+
+git config --local user.email "15393243613@163.com"
+
+git push -f https://${GITHUB_TOKEN}@github.com/haitaodesign/learning-resource.git master
