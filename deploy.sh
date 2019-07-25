0cb46b3c3e54dc5c816badef60d3182aba5ba557
haitaodesign <15393243613@163.com>
2019-07-26 00:22:47 +0800

    chore: git checkout dev

--- deploy.sh
+++ deploy.sh
@@ -13,4 +13,10 @@ git config --local user.name "haitaodesign"
 
 git config --local user.email "15393243613@163.com"
 
+git checkout -b dev
+
+git checkout master
+
+git merge dev
+
 git push -f https://${GITHUB_TOKEN}@github.com/haitaodesign/learning-resource.git master
