95482e378630f1a913e534e6dead59054accf1b6
haitaodesign <15393243613@163.com>
2019-07-26 00:50:08 +0800

    ci: git init

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,8 @@ set -e
 
 yarn build
 
+git init
+
 git add .
 
 git commit -m 'release: github pages'
@@ -12,16 +14,5 @@ git commit -m 'release: github pages'
 git config --local user.name "haitaodesign"
 
 git config --local user.email "15393243613@163.com"
-
-# 新建临时分支
-git checkout -b temp
-
-git checkout dev
-
-git merge temp
-
-git checkout -b master
-
-git merge dev
 
 git push -f https://${GITHUB_TOKEN}@github.com/haitaodesign/learning-resource.git master
